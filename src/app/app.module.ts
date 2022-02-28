99d8d1c5bd4b2f872b950c95fe19b68b18b81f3d
sukantahui <huisukanta77@gmail.com>
2022-02-28 13:32:01 +0530

    project cleanedup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,6 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
-// import { MatToolbarModule } from '@angular/material/toolbar';
-// import { MatMenuModule } from '@angular/material/menu';
-// import { MatIconModule} from '@angular/material/icon';
-// import { MatDividerModule } from '@angular/material/divider';
-// import { MatListModule } from '@angular/material/list';
-// import { MatCardModule } from '@angular/material/card';
-// import { MatTabsModule } from '@angular/material/tabs';
-// import { NgChartsModule } from 'ng2-charts';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatButtonModule } from '@angular/material/button';
-
-// import { ReactiveFormsModule } from '@angular/forms';
-// import { SidenavComponent } from './sidenav/sidenav.component';
 import {SidenavModule} from "./sidenavs/sidenav/sidenav.module";
 import {HeaderModule} from "./header/header.module";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
@@ -28,8 +15,6 @@ import {SidenavDeveloperModule} from "./sidenavs/sidenav-developer/sidenav-devel
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import { MAT_DATE_LOCALE} from "@angular/material/core";
-import {SidenavTutorialModule} from "./sidenavs/sidenav-tutorial/sidenav-tutorial.module";
-import {SidenavStudentModule} from "./sidenavs/sidenav-student/sidenav-student.module";
 import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 
@@ -52,8 +37,7 @@ import {HashLocationStrategy, LocationStrategy} from "@angular/common";
         SidenavDeveloperModule,
         FontAwesomeModule,
         MatProgressSpinnerModule,
-        SidenavTutorialModule,
-        SidenavStudentModule
+
     ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
