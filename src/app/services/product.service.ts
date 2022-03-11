2661ac6d20a42326bea772f4b80ae3bee7d81369
sukantahui <huisukanta77@gmail.com>
2022-03-11 23:50:55 +0530

    url updated

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { catchError, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ErrorService } from './error.service';
+import {CommonService} from "./common.service";
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,10 @@ export class ProductService {
   private BASE_API_URL = environment.BASE_API_URL;
 
 
-  constructor(private  http: HttpClient, private errorService: ErrorService) { }
+  constructor(private  http: HttpClient, private errorService: ErrorService, private commonService: CommonService) { }
 
   saveProducts(formdata: any){
-    return this.http.post(this.BASE_API_URL + "/products", formdata)
+    return this.http.post(this.commonService.getAPI() + "/products", formdata)
     .pipe(catchError(this.errorService.serverError), tap(response => {
       // console.log(response);
     }));
