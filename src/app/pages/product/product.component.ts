c925b6fb3368aae4aa78b6bf3c1ab8d5a7641b02
mounitbhandari <tataibhandari@gmail.com>
2022-03-28 11:24:58 +0530

    working on product

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -34,7 +34,7 @@ export class ProductComponent implements OnInit {
   productFormGroup= new FormGroup({
     id: new FormControl(),
     companyId: new FormControl(),
-    productName: new FormControl(null, [Validators.required, Validators.maxLength(10), Validators.minLength(4)]),
+    productName: new FormControl(null, [Validators.required, Validators.maxLength(50), Validators.minLength(4)]),
     description: new FormControl(null, [Validators.maxLength(100)]),
   });
   productList: any;
